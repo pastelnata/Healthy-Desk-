7e19663e1a0960d39a31481241d0a62d13b08925
Kuba <kubuniek11@gmail.com>
2024-10-30 21:37:10 +0100

    Initial score page commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeViewComponent } from './home/home-view/home-view.component';
 import { LoginViewComponent } from './login/login-view/login-view.component';
 import { AlertViewComponent } from './alert/alert-view/alert-view.component';
 import { AnalyticsViewComponent } from './analytics/analytics-view/analytics-view.component';
+import { ScoreViewComponent } from './score/score-view/score-view.component';
 
 const routes: Routes = [
   {path: '', component: HomeViewComponent},
   {path: 'login', component: LoginViewComponent},
   {path: 'alert', component: AlertViewComponent},
   {path: 'analytics', component: AnalyticsViewComponent},
+  {path: 'score', component: ScoreViewComponent},
 ];
 
 @NgModule({
---
+++ src/app/score/score-view/score-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ScoreModule } from '../score.module';
+@Component({
+  selector: 'app-score-view',
+  templateUrl: './score-view.component.html',
+  styleUrl: './score-view.component.css'
+})
+export class ScoreViewComponent {
+
+}
