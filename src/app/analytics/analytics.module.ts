47c9673c35db7c9f36bb873d4801e3f5980f7fcd
pastelnata <ritabb04.05@gmail.com>
2024-10-31 13:50:28 +0100

    Updated files - Added analytics component

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsViewComponent } from './analytics-view/analytics-view.component';
 import { NavigationModule } from '../navigation/navigation.module';
+import { AnalyticsComponent } from './analytics.component';
 
 
 @NgModule({
   declarations: [
-    AnalyticsViewComponent
+    AnalyticsViewComponent,
+    AnalyticsComponent
   ],
   imports: [
     CommonModule,
