5a43a2f51990b93e842792caa3d82a8f15545fad
pastelnata <ritabb04.05@gmail.com>
2024-10-31 15:10:20 +0100

    Add last 3 months in analytics

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsViewComponent } from './analytics-view/analytics-view.component';
 import { NavigationModule } from '../navigation/navigation.module';
-import { AnalyticsComponent } from './analytics.component';
-
 
 @NgModule({
   declarations: [
     AnalyticsViewComponent,
-    AnalyticsComponent
   ],
   imports: [
     CommonModule,
