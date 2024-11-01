c8ce4c9f89009527f86cbee823439a4ae710fa63
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-01 21:17:14 +0100

    added navigation visibility logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Healthy-Desk';
 
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkIfNavigationVisible();
+      }
+    });
+  }
+  
+  constructor(private router: Router) {}
+
   isAccountMenuVisible: boolean = false;
-  router: any;
+  
   
   isNavigationVisible: boolean = false;
 
