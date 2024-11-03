5d06e1c7a7361037e7ed3c0ef49cfce5ae588725
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-03 15:44:46 +0100

    Added leaderboard styling

--- src/app/score/score.service.ts
+++ src/app/score/score.service.ts
@@ -13,7 +13,39 @@ export class ScoreService {
   {id: 2,
     name: 'test2',
     score: 20
-  }];
+  },
+  {id: 3,
+    name: 'test3',
+    score: 21
+  },
+  {id: 4,   
+    name: 'test4',
+    score: 22
+  },
+  {id: 5,   
+    name: 'test5',
+    score: 23
+  },
+  {id: 6,   
+    name: 'test6',
+    score: 24
+  },
+  {id: 7,   
+    name: 'test7',
+    score: 25
+  },
+  {id: 8,   
+    name: 'test8',
+    score: 26
+  },
+  {id: 9,   
+    name: 'test9',
+    score: 27
+  },
+  {id: 10,   
+    name: 'test10',
+    score: 28
+  },];
   
   constructor() { }
 
