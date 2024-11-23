485b8dd871363c1e2a6a0bb57ae06b05690d55f4
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-23 18:13:26 +0100

    connected to picoW and led on/off

--- frontend/src/app/alert/alert-view/alert-view.component.ts
+++ frontend/src/app/alert/alert-view/alert-view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertModule } from '../alert.module';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-alert-view',
@@ -7,5 +8,23 @@ import { AlertModule } from '../alert.module';
   styleUrl: './alert-view.component.css'
 })
 export class AlertViewComponent {
-
+  param = "lighton?";
+  isLightOn = false;
+  apiUrl = 'http://192.168.0.104/';
+  constructor(private http: HttpClient) { }
+  toggleLight() {
+    console.log('Toggling light...');
+    if (this.isLightOn) {
+      this.isLightOn = false;
+      console.log("Light off");
+      this.param = "lightoff?";
+    }else{
+      this.isLightOn = true;
+      console.log("Light on");
+      this.param = "lighton?";
+    }
+    const url = `${this.apiUrl}${this.param}`;
+    this.http.get(url)
+      .subscribe();
+  }
 }
