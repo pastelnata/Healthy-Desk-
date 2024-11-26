ea6029c71ac54b749da72c43e999565dbbc2f8f8
Dobos23 <domimisb@gmail.com>
2024-11-26 01:43:44 +0100

    Working pie chart

--- frontend/src/app/analytics/analytics-view/analytics-view.component.ts
+++ frontend/src/app/analytics/analytics-view/analytics-view.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AnalyticsModule } from '../analytics.module';
+import { Chart } from 'angular-highcharts';
 @Component({
   selector: 'app-analytics-view',
-  templateUrl: './analytics-view.component.html',
+  templateUrl: './analytics-view.component.html', 
   styleUrl: './analytics-view.component.css'
+  
 })
 export class AnalyticsViewComponent {
   
@@ -13,6 +15,8 @@ export class AnalyticsViewComponent {
   public prevMonth: string;
   public twoMonthsAgo: string;
 
+  pieChart!: Chart;
+
   constructor() {
     const curDate = new Date();
     const curMonthIndex = curDate.getMonth();
@@ -20,5 +24,51 @@ export class AnalyticsViewComponent {
     this.curMonth = this.monthNames[curMonthIndex];
     this.prevMonth = this.monthNames[(curMonthIndex - 1 + 12) % 12];
     this.twoMonthsAgo = this.monthNames[(curMonthIndex - 2 + 12) % 12];
+
+  
+    this.pieChart = new Chart({
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Time Spent:',
+        style: {
+          color: 'rgb(85, 179, 211)', // Set your desired color (e.g., Hex, RGB, or named color)
+          fontSize: '18px', // Optional: Adjust font size
+          fontWeight: 'bold' // Optional: Adjust font weight
+        }
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+        point: {
+          valueSuffix: '%'
+        }
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [
+        {
+          type: 'pie',
+          name: 'Time Distribution',
+          data: [
+            { name: 'Standing', y: 40, color: '#36A2EB' },
+            { name: 'Sitting', y: 60, color: '#FF6384' }
+          ]
+        }
+      ]
+    });
   }
 }
+
+
+
