1ff8bc701e4ed08a4f67d684193980df873d52a5
sduhubert <husyl23@student.sdu.dk>
2024-11-26 01:44:51 +0100

    Manager menu (initial commit)

---
+++ frontend/src/manager/manager-view/manager-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-view',
+  templateUrl: './manager-view.component.html',
+  styleUrl: './manager-view.component.css'
+})
+export class ManagerViewComponent {
+
+}
---
+++ frontend/src/manager/manager.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { LoginViewComponent } from '../app/login/login-view/login-view.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManagerService {
+
+  constructor() { }
+
+  
+}
